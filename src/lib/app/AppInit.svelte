021bb44648bbcc5cc23132ace131ecc3712cd73d
theZilber <50196038+zilberlex@users.noreply.github.com>
2026-09-14 12:42:14 +0200

    Removed Redundent directory in hacker-blue theme

--- src/lib/app/AppInit.svelte
+++ src/lib/app/AppInit.svelte
@@ -13,9 +13,9 @@
 	import KeyboardNavigationManager from '$lib/packages/keyboard-navigation/svelte-components/KeyboardNavigationManager.svelte';
 	import { engineElementInteraction } from '$lib/engine/engine-hotkeys/engine-interactions';
 	import StyleLoader from '$lib/packages/ui/svelte/StyleLoader.svelte';
-	import HackerBlueTheme from '$lib/packages/ui/style/themes/hacker-blue/theme/svelte/HackerBlueTheme.svelte';
+	import HackerBlueTheme from '$lib/packages/ui/style/themes/hacker-blue/svelte/HackerBlueTheme.svelte';
 	import Elements3DLoadStyles from '$lib/engine/elements-3d/Elements3DLoadStyles.svelte';
-	import HackerBlueElements3DStyles from '$lib/packages/ui/style/themes/hacker-blue/theme/integrations/elements-3d/svelte/HackerBlueElements3DStyles.svelte';
+	import HackerBlueElements3DStyles from '$lib/packages/ui/style/themes/hacker-blue/integrations/elements-3d/svelte/HackerBlueElements3DStyles.svelte';
 
 	let { children } = $props();
 
