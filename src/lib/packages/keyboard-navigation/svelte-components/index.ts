f7b8f4d17503b7c152a5288ed4b662417de2ce37
theZilber <50196038+zilberlex@users.noreply.github.com>
2026-09-16 19:49:13 +0200

    added package indexing to keyboard-navigation

---
+++ src/lib/packages/keyboard-navigation/svelte-components/index.ts
@@ -0,0 +1,5 @@
+export { default as KeyboardNavigationManager } from './KeyboardNavigationManager.svelte';
+export { default as NavigationScope } from './NavigationScope.svelte';
+
+export { markForNavigation } from './attachments';
+export { assignNavigationManagerKeys } from './sveltekit-helpers';
